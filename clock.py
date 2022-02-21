4eec0c5f29c24be996860efe190edbee2be0343a
HristoShopski <99093281+HristoShopski@users.noreply.github.com>
2022-02-21 18:33:40 +0200

    Update and rename pac.py to clock.py

--- clock.py
+++ clock.py
@@ -6,12 +6,10 @@ label = Label(canvas, font=("ds-digital", 120, 'bold'), bg="black", fg="darkgree
 label.pack(anchor='center')
 label.grid(row=0, column=1)
 
-
 def digitalclock():
     text_input = time.strftime("%H:%M:%S")
     label.config(text=text_input)
     label.after(200, digitalclock)
-
 
 digitalclock()
 canvas.mainloop()
